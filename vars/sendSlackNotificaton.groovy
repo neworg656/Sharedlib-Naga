aefebb6c69486554bafddf0edfd101c29b3eb6c3
kkfunda2268256 <eswarjaya841@gmail.com>
2025-07-18 23:13:13 +0530

    Update sendSlackNotificaton.groovy

--- vars/sendSlackNotificaton.groovy
+++ vars/sendSlackNotificaton.groovy
@@ -1,1 +1,20 @@
+def call(String buildStatus = 'STARTED') {
+    buildStatus = buildStatus ?: 'SUCCESS'
 
+    def colorCode
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+
+    switch (buildStatus) {
+        case 'STARTED':
+            colorCode = '#FFFF00' // Yellow
+            break
+        case 'SUCCESS':
+            colorCode = '#00FF00' // Green
+            break
+        default:
+            colorCode = '#FF0000' // Red
+    }
+
+    slackSend(color: colorCode, message: summary, channel: '#jio-devops')
+}
